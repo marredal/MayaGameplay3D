549edc397e1ed93acdfa963752cefacfc679b540
martin <aston_martin93@hotmail.com>
2017-10-12 12:42:11 +0200

    fixed lib.

---
+++ MayaGameplay3D/Gameplay/src/GameplayDisplayer.h
@@ -0,0 +1,64 @@
+#ifndef GameplayDisplayer_H_
+#define GameplayDisplayer_H_
+
+#include <gameplay.h>
+
+using namespace gameplay;
+
+/**
+ * Main game class.
+ */
+class GameplayDisplayer: public Game
+{
+public:
+    
+
+    /**
+     * Constructor.
+     */
+    GameplayDisplayer();
+
+    /**
+     * @see Game::keyEvent
+     */
+	void keyEvent(Keyboard::KeyEvent evt, int key);
+	
+    /**
+     * @see Game::touchEvent
+     */
+    void touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex);
+
+protected:
+
+    /**
+     * @see Game::initialize
+     */
+    void initialize();
+
+    /**
+     * @see Game::finalize
+     */
+    void finalize();
+
+    /**
+     * @see Game::update
+     */
+    void update(float elapsedTime);
+
+    /**
+     * @see Game::render
+     */
+    void render(float elapsedTime);
+
+private:
+
+    /**
+     * Draws the scene each frame.
+     */
+    bool drawScene(Node* node);
+
+    Scene* _scene;
+    bool _wireframe;
+};
+
+#endif
