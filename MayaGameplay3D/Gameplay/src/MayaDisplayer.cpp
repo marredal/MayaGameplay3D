2797a5c28eefaf4a927305afd9a6eacda4912d1f
martin <aston_martin93@hotmail.com>
2017-10-12 10:24:51 +0200

    Set up project.

---
+++ MayaGameplay3D/Gameplay/src/MayaDisplayer.cpp
@@ -0,0 +1,80 @@
+#include "MayaDisplayer.h"
+
+// Declare our game instance
+MayaDisplayer game;
+
+MayaDisplayer::MayaDisplayer()
+    : _scene(NULL), _wireframe(false)
+{
+}
+
+void MayaDisplayer::initialize()
+{
+    // Load game scene from file
+    _scene = Scene::load("res/demo.scene");
+
+    // Get the box model and initialize its material parameter values and bindings
+    Node* boxNode = _scene->findNode("box");
+    Model* boxModel = dynamic_cast<Model*>(boxNode->getDrawable());
+    Material* boxMaterial = boxModel->getMaterial();
+
+    // Set the aspect ratio for the scene's camera to match the current resolution
+    _scene->getActiveCamera()->setAspectRatio(getAspectRatio());
+}
+
+void MayaDisplayer::finalize()
+{
+    SAFE_RELEASE(_scene);
+}
+
+void MayaDisplayer::update(float elapsedTime)
+{
+    // Rotate model
+    _scene->findNode("box")->rotateY(MATH_DEG_TO_RAD((float)elapsedTime / 1000.0f * 180.0f));
+}
+
+void MayaDisplayer::render(float elapsedTime)
+{
+    // Clear the color and depth buffers
+    clear(CLEAR_COLOR_DEPTH, Vector4::zero(), 1.0f, 0);
+
+    // Visit all the nodes in the scene for drawing
+    _scene->visit(this, &MayaDisplayer::drawScene);
+}
+
+bool MayaDisplayer::drawScene(Node* node)
+{
+    // If the node visited contains a drawable object, draw it
+    Drawable* drawable = node->getDrawable(); 
+    if (drawable)
+        drawable->draw(_wireframe);
+
+    return true;
+}
+
+void MayaDisplayer::keyEvent(Keyboard::KeyEvent evt, int key)
+{
+    if (evt == Keyboard::KEY_PRESS)
+    {
+        switch (key)
+        {
+        case Keyboard::KEY_ESCAPE:
+            exit();
+            break;
+        }
+    }
+}
+
+void MayaDisplayer::touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex)
+{
+    switch (evt)
+    {
+    case Touch::TOUCH_PRESS:
+        _wireframe = !_wireframe;
+        break;
+    case Touch::TOUCH_RELEASE:
+        break;
+    case Touch::TOUCH_MOVE:
+        break;
+    };
+}
